c13677b915207d72efc18b5db6a1cebb0e2ec1e3
Poteryaev Aleksandr <sp202037@yandex.ru>
2024-09-22 16:21:56 +0300

    Updated to v.1.1.5
    Added production version of the project.

--- cabinet.php
+++ cabinet.php
@@ -16,7 +16,7 @@ if (!isset($_COOKIE['email']) or trim($_COOKIE['email']) == '') {
     <link rel="shortcut icon" href="favicon.png" type="image/png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
-    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link type="text/css" rel="stylesheet" href="css/style.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>User cabinet</title>
 </head>
@@ -145,9 +145,9 @@ if (!isset($_COOKIE['email']) or trim($_COOKIE['email']) == '') {
     </div>
 
     <script src="js/materialize.js"></script>
-    <script src="script/ajax.js"></script>
-    <script src="script/get_user_data.js"></script>
-    <script src="script/logout.js"></script>
+    <script src="script/ajax.min.js"></script>
+    <script src="script/get_user_data.min.js"></script>
+    <script src="script/logout.min.js"></script>
 </body>
 
 </html>
